88112e10e960f6b26d551dbf80102dba87ad28d1
khadijahazward <khadijahazward@gmail.com>
2023-11-24 11:49:01 +0530

    feat: asgardeo update

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,8 +1,14 @@
 import { Typography } from "@mui/material";
 import { useAuthContext } from "@asgardeo/auth-react";
+import { useEffect } from "react";
 
 function Index(){
     const { state, signIn, signOut } = useAuthContext();
+    
+    useEffect(() => {
+        console.log(state);
+    }, [state]);
+
 
     return (
         <div className="App">
@@ -12,6 +18,8 @@ function Index(){
                 <div>
                   <ul>
                     <li>{state.username}</li>
+                    <li>{state.displayName}</li>
+                    <li>helo</li>
                   </ul>
     
                   <button onClick={() => signOut()}>Logout</button>
