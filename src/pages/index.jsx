1bbdba033630ff34c9783a4eec52f7a759acadbd
khadijahazward <khadijahazward@gmail.com>
2023-11-27 13:20:20 +0530

    Fix: asgardeo issue

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,33 +1,55 @@
-import { Typography } from "@mui/material";
+import { useEffect, useState } from "react";
 import { useAuthContext } from "@asgardeo/auth-react";
-import { useEffect } from "react";
 
-function Index(){
-    const { state, signIn, signOut } = useAuthContext();
-    
+function Index() {
+    const { state, signOut, getAccessToken, signIn } = useAuthContext();
+    const [userInfo, setUserInfo] = useState(null);
+
     useEffect(() => {
-        console.log(state);
-    }, [state]);
+        const fetchData = async () => {
+            try {
+                const response = await fetch('https://api.asgardeo.io/t/wso2khadijah/oauth2/userinfo', {
+                    headers: {
+                        Authorization: `Bearer ${await getAccessToken()}`
+                    }
+                });
 
+                if (response.ok) {
+                    const json = await response.json();
+                    setUserInfo(json);
+                } else {
+                    console.error('Error:', response.statusText);
+                }
+            } catch (error) {
+                console.error('Error:', error);
+            }
+        };
+        fetchData();
+        console.log("user info", userInfo);
+    }, [getAccessToken]);
 
     return (
         <div className="App">
-          {
-            state.isAuthenticated
-              ? (
+            {state.isAuthenticated ? (
                 <div>
-                  <ul>
-                    <li>{state.username}</li>
-                    <li>{state.displayName}</li>
-                    <li>{state.sub}</li>
-                    <li>helo</li>
-                  </ul>
-    
-                  <button onClick={() => signOut()}>Logout</button>
+                    <ul>
+                        <li>{state.username}</li>
+                        <li>{state.displayName}</li>
+                        <li>{state.sub}</li>
+
+                        {userInfo && (
+                            <>
+                                <li>birthdate: {userInfo.birthdate}</li>
+                                <li>role: {userInfo.application_roles}</li>
+                            </>
+                        )}
+                    </ul>
+
+                    <button onClick={() => signOut()}>Logout</button>
                 </div>
-              )
-              : <button onClick={() => signIn()}>Lets Get Started</button>
-          }
+            ) : (
+                <button onClick={() => signIn()}>Let's Get Started</button>
+            )}
         </div>
     );
 }
