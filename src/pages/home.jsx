5e9d04305feb54a5438c29e27728079993be9653
khadijahazward <khadijahazward@gmail.com>
2023-11-28 19:35:11 +0530

    fix: refresh screen issue

---
+++ src/pages/home.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+function Home (){
+    return(
+        <h1>home page for all</h1>
+    );
+}
+
+export default Home;
--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,24 +1,49 @@
-import { useEffect, useState } from "react";
+import React, { useEffect, useState } from "react";
 import Header from "../components/Header";
 import { useAuthContext } from "@asgardeo/auth-react";
 import RefreshScreen from "./refresh";
+import Home from "./home";
+import LinearProgress from '@mui/joy/LinearProgress';
+import Box from '@mui/joy/Box';
+import Grid from "@mui/joy/Grid";
 
 function Index() {
-    const { state, signIn } = useAuthContext();
-    //const [userInfo, setUserInfo] = useState(null);
+  const { state } = useAuthContext();
+  const [authenticatedAfterDelay, setAuthenticatedAfterDelay] = useState(false);
 
-    return (
-        <>
-        <Header secured={false} />
-        <div className="App">
-            {state.isAuthenticated ? (
-                <RefreshScreen />
-            ) : (
-                <button onClick={() => signIn()}>Let's Get Started</button>
-            )}
-        </div>
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      setAuthenticatedAfterDelay(true);
+    }, 2500);
+
+    return () => clearTimeout(timer);
+  }, []);
+
+  return (
+    <>
+      <Header secured={false} />
+      <div className="App">
+        {authenticatedAfterDelay ? (
+          state.isAuthenticated ? (
+            <RefreshScreen />
+          ) : (
+            <Home />
+          )
+        ) : (
+            <Box sx={{ height: '88vh', display: 'flex', justifyContent: 'center', flexGrow: 1 }}>
+            <Grid container spacing={2} columns={16} style={{ margin: 0, alignItems: 'center' }} sx={{ flexGrow: 1 }}>
+                <Grid xs={4}></Grid>
+                <Grid xs={8} style={{ backgroundColor: 'rgba(255, 118, 27, 0.9)', padding: '20px', borderRadius: '8px', textAlign: 'center'}}>
+                    <LinearProgress color="neutral" determinate={false} size="lg" variant="soft" />
+                    <h1 style={{ marginTop: '20px', fontFamily: 'Arial, sans-serif', fontSize: '24px', fontWeight: 'bold', color:'white' }}>Loading...</h1>
+                </Grid>
+                <Grid xs={4}></Grid>
+            </Grid>
+            </Box>
+        )}
+      </div>
     </>
-    );
+  );
 }
 
 export default Index;
