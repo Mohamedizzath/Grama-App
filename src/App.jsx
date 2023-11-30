d69b2521f2538df5aadb0c6035e87624fbef8e5f
mohamedizzath <mohamedizzath2016@gmail.com>
2023-11-30 13:39:50 +0530

    fix: Refactored

--- src/App.jsx
+++ src/App.jsx
@@ -6,7 +6,6 @@ import '@fontsource/roboto/700.css';
 import { Typography } from '@mui/material';
 import { RouterProvider } from 'react-router-dom';
 import { AuthProvider, SecureRoute } from "@asgardeo/auth-react";
-import config from './config/asgardeo-config';
 import router from './config/router';
 import { BrowserRouter, Router, Routes, Route } from "react-router-dom";
 import { CssVarsProvider, ThemeProvider } from '@mui/joy';
@@ -24,5 +23,14 @@ function App() {
     </>
   )
 }
+
+const config = {
+  signInRedirectURL: `${import.meta.env.VITE_ASGARDEO_SIGNIN_URL}`,
+  signOutRedirectURL: `${import.meta.env.VITE_ASGARDEO_SIGNOUT_URL}`,
+  clientID: `${import.meta.env.VITE_ASGARDEO_CLIENT_ID}`,
+  baseUrl: `${import.meta.env.VITE_ASGARDEO_BASE_URL}`,
+  scope: [ "openid","profile", "app_roles", "email","phone"],
+  "storage": "sessionStorage"
+};
 
 export default App
