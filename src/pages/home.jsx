3fc20cd6f96ffc504ca51184360b4031e3c959ac
khadijahazward <khadijahazward@gmail.com>
2023-11-30 07:24:58 +0530

    fix: home screen video issue

--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -1,12 +1,11 @@
 import React, { useRef } from 'react';
-import videoFile from '../assets/test-video.mp4';
+import videoFile from '../assets/introduction-3.mp4';
 import Header from "../components/Header";
 import Grid from "@mui/joy/Grid";
 import Container from '@mui/system/Container';
 import ReactPlayer from 'react-player';
-import {maxWidth } from '@mui/system';
 import { useAuthContext } from "@asgardeo/auth-react";
-import { Typography } from '@mui/material';
+import { Typography } from '@mui/joy';
 import Button from '@mui/joy/Button';
 
 function Home() {
@@ -16,21 +15,33 @@ function Home() {
     <Container maxWidth={false} disableGutters style={{ height: '100vh',overflow: 'hidden'}}>
       <Header secured={false} />
       <Grid container spacing={0} columns={16} style={{height:'calc(100vh - 68px)'}}>
-        <Grid  xs={16} sm={16} md={8} lg={8} backgroundColor='black'>
-            <ReactPlayer ref={playerRef} url={videoFile} controls={false} playing={true}
-            loop={true}  width={maxWidth} muted={true}  style={{ minHeight: '100%' }} />
+        <Grid  xs={16} sm={16} md={8} lg={8}>
+            <ReactPlayer ref={playerRef} url={videoFile} controls={false} playing={true}  
+            loop={true} muted={true}  style={{ minHeight: '100%', minWidth: '100%', 
+            transform: 'scaleX(1.22)',
+            // '@media (max-width: 600px)': {
+            //     transform: 'scaleX(1)', // xs screens
+            // },
+            // '@media (min-width: 601px) and (max-width: 960px)': {
+            //     transform: 'scaleX(1.15)', // sm screens
+            // },
+            // '@media (min-width: 961px) and (max-width: 1280px)': {
+            //     transform: 'scaleX(1.18)', // md screens
+            // },
+            }} 
+            />
         </Grid>
         <Grid  xs={16} sm={16} md={8} lg={8} style={{padding: '20px', display:'flex', alignItems:'center', justifyContent:'center' }}>
             <div style={{ textAlign: 'center'}}>
-                <Typography variant="h1" style={{marginBottom:'1px'}}>
+                <Typography level="h1" fontSize={'40px'} style={{marginBottom:'1px'}} color='main'>
                     Get Started
                 </Typography>
-                <Typography variant="body-sm">
+                <Typography level="body-sm" fontSize={'15px'} color='neutral' >
                     Explore our platform and discover amazing features.
                 </Typography>
 
                 <div style={{ marginTop: '20px' }}>
-                    <Button variant="filled" color="primary" style={{ backgroundColor: 'red', color: 'white', padding:'10px', borderRadius:'5px', width:'50%' }} 
+                    <Button variant="filled" style={{ backgroundColor: 'red', color: 'white', padding:'10px', borderRadius:'5px', width:'50%' }} 
                     onClick={() => signIn()}>Log In
                     </Button>
                 </div>
