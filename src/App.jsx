e421734bed058f0208149539419bba93c81dbdc6
mohamedizzath <mohamedizzath2016@gmail.com>
2023-11-30 14:06:17 +0530

    fix: Add asgardeo configs

--- src/App.jsx
+++ src/App.jsx
@@ -25,10 +25,10 @@ function App() {
 }
 
 const config = {
-  signInRedirectURL: `${import.meta.env.VITE_ASGARDEO_SIGNIN_URL}`,
-  signOutRedirectURL: `${import.meta.env.VITE_ASGARDEO_SIGNOUT_URL}`,
-  clientID: `${import.meta.env.VITE_ASGARDEO_CLIENT_ID}`,
-  baseUrl: `${import.meta.env.VITE_ASGARDEO_BASE_URL}`,
+  signInRedirectURL: `https://f0f85940-267a-4c31-b011-c56edd21bdea.e1-us-east-azure.choreoapps.dev`,
+  signOutRedirectURL: `https://f0f85940-267a-4c31-b011-c56edd21bdea.e1-us-east-azure.choreoapps.dev`,
+  clientID: `fzo2NWtoZMhrklIDf95yz9TjkqQa`,
+  baseUrl: `https://api.asgardeo.io/t/wso2khadijah`,
   scope: [ "openid","profile", "app_roles", "email","phone"],
   "storage": "sessionStorage"
 };
