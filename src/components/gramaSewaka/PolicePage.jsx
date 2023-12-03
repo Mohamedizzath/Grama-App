01d5b22acf21762308a51d2750bc847c4f3285ae
khadijahazward <khadijahazward@gmail.com>
2023-12-03 21:51:57 +0530

    feat: address and police check requests

--- src/components/gramaSewaka/PolicePage.jsx
+++ src/components/gramaSewaka/PolicePage.jsx
@@ -1,15 +1,131 @@
 import React from "react";
 import SubHeader from "./subHeader";
-import {Box} from "@mui/joy";
+import { Box, Typography, Table, Select, Option, Sheet, Button  } from "@mui/joy";
+import KeyboardArrowLeftIcon from '@mui/icons-material/KeyboardArrowLeft';
+import KeyboardArrowRightIcon from '@mui/icons-material/KeyboardArrowRight';
+import policeRequests from "../../test-data/policeRequests";
+import { useState, useEffect } from "react";
+import ViewPoliceModal from "./ViewPoliceModal";
 
 function PolicePage(){
+
+    const rowsPerPage = 4;
+
+    const [idReqs, setIdReqs] = useState(policeRequests);
+    const [viewReqs, setViewReq] = useState([]);
+    const [viewStatus, setViewStatus] = useState("ALL");
+    const [showingDetail, setShowingDetail] = useState(null);
+    const [currentPage, setCurrentPage] = useState(1);
+    const [totalPages, setTotalPages] = useState(1);
+    const [showReq, setShowReq] = useState(false);
+
+    useEffect(() => {
+        const filterRequests = () => {
+            if (viewStatus === "PENDING") {
+                return idReqs.filter((req) => req["status"] === "PENDING");
+            } else if (viewStatus === "VERIFIED") {
+                return idReqs.filter((req) => req["status"] === "VERIFIED");
+            } else if (viewStatus === "REJECTED") {
+                return idReqs.filter((req) => req["status"] === "REJECTED");
+            } else {
+                return idReqs;
+            }
+        };
+
+        const filteredReqs = filterRequests();
+        const totalItems = filteredReqs.length; 
+        const calculatedTotalPages = Math.ceil(totalItems / rowsPerPage); 
+
+        setTotalPages(calculatedTotalPages); 
+
+        const startIndex = (currentPage - 1) * rowsPerPage;
+        const endIndex = startIndex + rowsPerPage;
+        const slicedReqs = filteredReqs.slice(startIndex, endIndex);
+
+        setViewReq(slicedReqs);
+    }, [viewStatus, idReqs, currentPage]);
+    
+
+    // view details function
+    function showDetailRequest(details) {
+        setShowingDetail(details);
+        setShowReq(true);
+    }
+
+    function handlePageChange(event, value) {
+        if (value > 0 && value <= totalPages) {
+            setCurrentPage(value);
+        }
+    }
+
 
     return(
         <Box>
             <SubHeader />
-            <Box paddingY={'1.5%'}>
-                identity
-            </Box>       
+            <Box display="flex" justifyContent="space-between" alignItems="flex-end" sx={{ marginTop: "18px", marginBottom: "8px" }}>
+                <Typography level="h2">Police Requests</Typography>
+                <Select size="sm" defaultValue={viewStatus} onChange={(event,value) => setViewStatus(value)} sx={{ width: "180px"}}>
+                    <Option key="ALL" value="ALL">All requests</Option>
+                    <Option key="PENDING" value="PENDING">Pending requests</Option>
+                    <Option key="VERIFIED" value="VERIFIED">Confirmed requests</Option>
+                    <Option key="REJECTED" value="REJECTED">Rejected requests</Option>
+                </Select>
+            </Box>
+            <Sheet
+                sx={{
+                    '--TableCell-height': '40px',
+                    '--TableHeader-height': 'calc(1 * var(--TableCell-height))',
+                    height: 300,
+                    overflow: 'auto',
+                    backgroundSize: '100% 40px, 100% 40px, 100% 14px, 100% 14px',
+                    backgroundRepeat: 'no-repeat',
+                    backgroundAttachment: 'local, local, scroll, scroll',
+                    backgroundPosition: '0 var(--TableHeader-height), 0 100%, 0 var(--TableHeader-height), 0 100%',
+                    backgroundColor: '#FFFFFF',           
+                }}
+            >
+                <Table stickyHeader>
+                    <thead>
+                        <tr>
+                            <th style={{ textAlign: 'center' }}><Typography level="h5" sx={{color:'#F26202'}}>Applied Date</Typography></th>
+                            <th style={{ textAlign: 'center' }}><Typography level="h5" sx={{color:'#F26202'}}>NIC</Typography></th>
+                            <th style={{ textAlign: 'center' }}><Typography level="h5" sx={{color:'#F26202'}}>Action</Typography></th>
+                        </tr>
+                    </thead>
+                    <tbody style={{ overflowY: 'auto', maxHeight: '300px' }}>
+                        {viewReqs.map((req, index) => (
+                            <tr key={index} style={{ textAlign: 'center' }}>
+                                <td><Typography level="body-sm">{req["applied-date"]}</Typography></td>
+                                <td><Typography level="body-sm">{req.nic}</Typography></td>
+                                <td>
+                                    <Button color="main" variant="outlined" sx={{ maxHeight: '3px', paddingX: '10px', fontSize:'10px' }} onClick={() => showDetailRequest(req)}>View Details</Button>
+                                </td>
+                            </tr>
+                        ))}
+                    </tbody>
+                </Table>
+                {/* Individual request showing modal */}
+                {
+                    showReq && <ViewPoliceModal viewOpen={showReq} setViewOpen={setShowReq} details={showingDetail} />
+                }
+            </Sheet>  
+            <div style={{ display: 'flex', alignItems: 'center', justifyContent: 'center', marginTop: '1rem' }}>
+                <Button variant="outlined" size="small" color="main"
+                    disabled={currentPage === 1}
+                    onClick={(e) => handlePageChange(e, currentPage - 1)}
+                    sx={{ marginRight: '8px' }}
+                >
+                    <KeyboardArrowLeftIcon fontSize="small" />
+                </Button>
+                <Typography level="body-xs">Page {currentPage} of {totalPages}</Typography>
+                <Button variant="outlined" size="small" color="main"
+                    disabled={currentPage >= totalPages}
+                    onClick={(e) => handlePageChange(e, currentPage + 1)}
+                    sx={{ marginLeft: '8px' }}
+                >
+                    <KeyboardArrowRightIcon fontSize="small" />
+                </Button>
+            </div>
         </Box>
     );
 }
---
+++ src/components/gramaSewaka/ViewAddressModal.jsx
@@ -0,0 +1,155 @@
+import { Modal, ModalDialog, Box, Typography, Grid, FormControl, FormLabel, Input, Button, Select, Option, Chip } from "@mui/joy";
+import CloseIcon from '@mui/icons-material/Close';
+import CheckIcon from '@mui/icons-material/Check';
+import AlarmIcon from '@mui/icons-material/Alarm';
+import SaveIcon from '@mui/icons-material/Save'; 
+import LocationOnIcon from '@mui/icons-material/LocationOn';
+import React from "react";
+import { useState } from "react";
+
+function ViewAddressModal({ viewOpen, setViewOpen, details }){
+    const [selectedStatus, setSelectedStatus] = useState((details["status"]));
+
+    let headerTheme = "primary";
+    let chipIcon = null;
+    let chipDisplay = "Pending";
+
+    if(details["status"] === "VERIFIED"){
+        headerTheme = "success";
+        chipIcon = <CheckIcon/>
+        chipDisplay = "Verified";
+    } else if(details["status"] === "REJECTED"){
+        headerTheme = "danger";
+        chipIcon = <CloseIcon />
+        chipDisplay = "Rejected"
+    } else {
+        headerTheme = "primary";
+        chipIcon = <AlarmIcon />
+        chipDisplay = "Pending";
+    }
+
+    const handleStatusChange = (event, newValue) => {
+        setSelectedStatus(newValue);
+    };
+
+    // handle status change when saving modal
+    const handleSave = () => {
+        console.log("Status Changed:", selectedStatus);
+        setViewOpen(false);
+    };
+
+    return <>
+        <Modal open={viewOpen} onClose={() => setViewOpen(false)}>
+                <ModalDialog sx={{ padding: "0px",overflowX: "hidden", overflowY: "auto" }}>
+                    <Box sx={{ height: "15px",
+                    background: (theme) =>
+                    `linear-gradient(to top, ${theme.vars.palette[headerTheme][800]}, ${theme.vars.palette[headerTheme][500]})`,    
+                    }}></Box>
+                    <Box sx={{ padding: "16px"}}>
+                        <Box display="flex" alignItems="center" justifyContent="flex-start">
+                            <LocationOnIcon color="neutral" fontSize="sm" />
+                            &nbsp;
+                            <Typography level="body-md">Address Request Details</Typography>
+                        </Box>
+                        
+                        <Box display="flex" justifyContent="space-between" alignItems="center">
+                            <Typography level="h2">Applied date: {details["applied-date"]}</Typography>
+                            <Chip color={headerTheme} startDecorator={chipIcon} sx={{ paddingX: "16px", paddingY: "4px"}}>{chipDisplay}</Chip>
+                        </Box>
+                        
+                        <Grid container spacing={2} sx={{ flexGrow: 1, marginTop: "16px" }}>
+                            <Grid md={12}>
+                                <FormControl>
+                                    <FormLabel>Address</FormLabel>
+                                    <Input value={details["address"]}/> 
+                                </FormControl>
+                            </Grid>
+                            <Grid xs={12} md={6}>
+                                <FormControl>
+                                    <FormLabel>NIC Number</FormLabel>
+                                    <Input value={details["nic"]}/> 
+                                </FormControl>
+                            </Grid>
+
+                            <Grid xs={12} md={6}>
+                                <FormControl>
+                                    <FormLabel>Identity Approved By</FormLabel>
+                                    <Input value={details["identity-check-approved-by"]} />
+                                </FormControl>
+                            </Grid>
+
+                            <Grid xs={12} md={6}>
+                                <FormControl>
+                                    <FormLabel>Identity Approved Date</FormLabel>
+                                    <Input value={details["identity-check-approved-date"]} />
+                                </FormControl>
+                            </Grid>
+
+                            {/* allow status change only if status is pending */}
+                            {
+                                details["status"] === "PENDING" && (
+                                    <Grid xs={12} md={6}>
+                                        <FormControl>
+                                            <FormLabel>Status</FormLabel>
+                                            <Select defaultValue={selectedStatus} onChange={handleStatusChange} sx={{ backgroundColor: 'lightgray' }}>
+                                            <Option value="PENDING" sx={{ color: 'gray', pointerEvents: 'none' }}>
+                                                Pending
+                                            </Option>
+                                                <Option value="VERIFIED" sx={{ color: 'green' }}>
+                                                    Verify
+                                                </Option>
+                                                <Option value="REJECTED" sx={{ color: 'red' }}>
+                                                    Reject
+                                                </Option>
+                                            </Select>
+                                        </FormControl>
+                                    </Grid>
+                                )
+                            }
+
+                            {
+                                details["status"] !== "PENDING" && (
+                                    <Grid xs={12} md={12}>
+                                        <Typography level="body-sm">Request status details</Typography>
+                                        {
+                                            details["status"] === "VERIFIED" && (
+                                                <Box display="flex" justifyContent="space-between" alignItems="center">  
+                                                    <Box display="flex" flexDirection="column">
+                                                        <Typography level="h4">Approved by - {details["approved-by"]["name"]}</Typography>
+                                                        <Typography level="body-sm">Approved date - {details["approved-by"]["approved-date"]}</Typography> 
+                                                        </Box>
+                                                </Box>
+                                            )
+                                        }
+                                        {
+                                            details["status"] === "REJECTED" && (
+                                                <Box display="flex" justifyContent="space-between" alignItems="center">  
+                                                    <Box display="flex" flexDirection="column">
+                                                        <Typography level="h4">Rejected by - {details["approved-by"]["name"]}</Typography>
+                                                        <Typography level="body-sm">Rejected date - {details["approved-by"]["approved-date"]}</Typography> 
+                                                    </Box>
+                                                </Box>
+                                            )
+                                        }
+                                    </Grid>
+                                )
+                            }
+                            <Grid md={12} display="flex" justifyContent="center" sx={{ marginTop: "12px"}}>
+                                {
+                                    details["status"] === "PENDING" && (
+                                        <>
+                                            <Button color={headerTheme} variant="solid" startDecorator={<SaveIcon />} sx={{ marginLeft: "8px" }} onClick={handleSave}>Save</Button>
+                                        </>
+                                    )
+                                }
+
+                                <Button color={headerTheme} variant="solid" startDecorator={<CloseIcon/>} sx={{ marginLeft: "8px" }} onClick={() => setViewOpen(false)}>Close</Button>
+                            </Grid>
+                        </Grid>
+                    </Box>
+                </ModalDialog>
+            </Modal>
+    </>;
+}
+
+export default ViewAddressModal;
