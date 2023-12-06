e28ab4d4584325d3a6a61e5ea98b9c0907d50a75
mohamedizzath <mohamedizzath2016@gmail.com>
2023-12-06 09:38:16 +0530

    feat: Add development configs

--- config.js
+++ config.js
@@ -1,8 +1,8 @@
 window.config = {
     apiGatewayUrl: "https://cf3a4176-54c9-4547-bcd6-c6fe400ad0d8-dev.e1-us-east-azure.choreoapis.dev/vzdb/gdki-api-gateway/endpoint-9090-803/v1.0",
     auth: {
-        signInRedirectURL: "http://localhost:5173/",
-        signOutRedirectURL: "http://localhost:5173/",
+        signInRedirectURL: "https://c3e60b00-766b-485e-88a1-033d2654116e.e1-us-east-azure.choreoapps.dev",
+        signOutRedirectURL: "https://c3e60b00-766b-485e-88a1-033d2654116e.e1-us-east-azure.choreoapps.dev",
         clientID: "QbwCeuX54LB41vhsUqfutlR8gpUa",
         baseUrl: "https://api.asgardeo.io/t/interns",
     }
